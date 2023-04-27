2ec67e9416a449aeb3980981d153fbfb62323af3
Kailash Panda <raaj.prince2016@gmail.com>
2023-04-27 20:48:59 +0530

    my 2nd commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,18 +1,36 @@
 package com.example.demo.controller;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class EmployeeController {
+
+@RestController   //@Controller + @ResponseBody
+public class EmployeeController { 
 	
-	@RequestMapping(value = "/employees", method = RequestMethod.GET)
-	@ResponseBody
+	
+	@GetMapping("/employees")
 	public String getEmployees() {
-		return "Hello Spring Booter";
+		return "displaying the list of employees";
+	}
+	@GetMapping("/employees/{id}")
+	public String getEmployee(@PathVariable Long id) {
+		return "Fetching the employee details for the id "+id;
+	}
+	@PostMapping("/employees")
+	public String saveEmployee(@RequestBody Employee employee) {
+		return"saving the employee details to the database"+employee;
+		
+	}
+	@DeleteMapping("/employees")
+	public String deleteEmployee(@RequestParam Long id) {
+		return"Deleting the employee details for the id "+id;
+		
 	}
 
 }
